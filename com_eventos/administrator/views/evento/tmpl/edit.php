8ae507769ab3f0fd0397cd80f405f4c9f9bc3edb
Lisandro Silva <lisandro.t.silva@gmail.com>
2020-11-30 19:32:45 -0300

    Ajustes de obrigatoriedade do e-mail da inscrição

--- com_eventos/administrator/views/evento/tmpl/edit.php
+++ com_eventos/administrator/views/evento/tmpl/edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @version    CVS: 1.0.0
  * @package    Com_Eventos
@@ -15,7 +16,6 @@ use \Joomla\CMS\Uri\Uri;
 use \Joomla\CMS\Router\Route;
 use \Joomla\CMS\Language\Text;
 
-
 HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 HTMLHelper::_('behavior.tooltip');
 HTMLHelper::_('behavior.formvalidation');
@@ -28,38 +28,33 @@ $document->addStyleSheet(Uri::root() . 'media/com_eventos/css/form.css');
 ?>
 <script type="text/javascript">
 	js = jQuery.noConflict();
-	js(document).ready(function () {
-		
+	js(document).ready(function() {
+
 	});
 
-	Joomla.submitbutton = function (task) {
+	Joomla.submitbutton = function(task) {
 		if (task == 'evento.cancel') {
 			Joomla.submitform(task, document.getElementById('evento-form'));
-		}
-		else {
-			
+		} else {
+
 			if (task != 'evento.cancel' && document.formvalidator.isValid(document.id('evento-form'))) {
-				
+
 				Joomla.submitform(task, document.getElementById('evento-form'));
-			}
-			else {
+			} else {
 				alert('<?php echo $this->escape(Text::_('JGLOBAL_VALIDATION_FORM_FAILED')); ?>');
 			}
 		}
 	}
 </script>
 
-<form
-	action="<?php echo JRoute::_('index.php?option=com_eventos&layout=edit&id=' . (int) $this->item->id); ?>"
-	method="post" enctype="multipart/form-data" name="adminForm" id="evento-form" class="form-validate form-horizontal">
-
-	
+<form action="<?php echo JRoute::_('index.php?option=com_eventos&layout=edit&id=' . (int) $this->item->id); ?>" method="post" enctype="multipart/form-data" name="adminForm" id="evento-form" class="form-validate form-horizontal">
 	<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'evento')); ?>
 	<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'evento', JText::_('COM_EVENTOS_TAB_EVENTO', true)); ?>
 	<div class="row-fluid">
 		<div class="span10 form-horizontal">
 			<fieldset class="adminform">
 				<legend><?php echo JText::_('COM_EVENTOS_FIELDSET_EVENTO'); ?></legend>
+				<?php echo $this->form->renderField('state'); ?>
 				<?php echo $this->form->renderField('nome'); ?>
 				<?php echo $this->form->renderField('nome_interno'); ?>
 				<?php echo $this->form->renderField('descricao'); ?>
@@ -78,16 +73,13 @@ $document->addStyleSheet(Uri::root() . 'media/com_eventos/css/form.css');
 	<?php echo JHtml::_('bootstrap.endTab'); ?>
 	<input type="hidden" name="jform[id]" value="<?php echo $this->item->id; ?>" />
 	<input type="hidden" name="jform[ordering]" value="<?php echo $this->item->ordering; ?>" />
-	<input type="hidden" name="jform[state]" value="<?php echo $this->item->state; ?>" />
 	<input type="hidden" name="jform[checked_out]" value="<?php echo $this->item->checked_out; ?>" />
 	<input type="hidden" name="jform[checked_out_time]" value="<?php echo $this->item->checked_out_time; ?>" />
 	<?php echo $this->form->renderField('created_by'); ?>
 	<?php echo $this->form->renderField('modified_by'); ?>
 
-	
 	<?php echo JHtml::_('bootstrap.endTabSet'); ?>
 
-	<input type="hidden" name="task" value=""/>
+	<input type="hidden" name="task" value="" />
 	<?php echo JHtml::_('form.token'); ?>
-
 </form>
--- com_inscritos/administrator/views/inscritos/tmpl/edit.php
+++ com_inscritos/administrator/views/inscritos/tmpl/edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @version    CVS: 1.0.0
  * @package    Com_Inscritos
@@ -28,55 +29,46 @@ $document->addStyleSheet(Uri::root() . 'media/com_inscritos/css/form.css');
 ?>
 <script type="text/javascript">
 	js = jQuery.noConflict();
-	js(document).ready(function () {
-		
-	js('input:hidden.evento_id').each(function(){
-		var name = js(this).attr('name');
-		if(name.indexOf('evento_idhidden')){
-			js('#jform_evento_id option[value="'+js(this).val()+'"]').attr('selected',true);
-		}
-	});
-	js("#jform_evento_id").trigger("liszt:updated");
+	js(document).ready(function() {
+
+		js('input:hidden.evento_id').each(function() {
+			var name = js(this).attr('name');
+			if (name.indexOf('evento_idhidden')) {
+				js('#jform_evento_id option[value="' + js(this).val() + '"]').attr('selected', true);
+			}
+		});
+		js("#jform_evento_id").trigger("liszt:updated");
 	});
 
-	Joomla.submitbutton = function (task) {
+	Joomla.submitbutton = function(task) {
 		if (task == 'inscritos.cancel') {
 			Joomla.submitform(task, document.getElementById('inscritos-form'));
-		}
-		else {
-			
+		} else {
 			if (task != 'inscritos.cancel' && document.formvalidator.isValid(document.id('inscritos-form'))) {
-				
 				Joomla.submitform(task, document.getElementById('inscritos-form'));
-			}
-			else {
+			} else {
 				alert('<?php echo $this->escape(Text::_('JGLOBAL_VALIDATION_FORM_FAILED')); ?>');
 			}
 		}
 	}
 </script>
 
-<form
-	action="<?php echo JRoute::_('index.php?option=com_inscritos&layout=edit&id=' . (int) $this->item->id); ?>"
-	method="post" enctype="multipart/form-data" name="adminForm" id="inscritos-form" class="form-validate form-horizontal">
-
-	
+<form action="<?php echo JRoute::_('index.php?option=com_inscritos&layout=edit&id=' . (int) $this->item->id); ?>" method="post" enctype="multipart/form-data" name="adminForm" id="inscritos-form" class="form-validate form-horizontal">
 	<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'ingressos')); ?>
 	<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'ingressos', JText::_('COM_INSCRITOS_TAB_INGRESSOS', true)); ?>
 	<div class="row-fluid">
 		<div class="span10 form-horizontal">
 			<fieldset class="adminform">
 				<legend><?php echo JText::_('COM_INSCRITOS_FIELDSET_INGRESSOS'); ?></legend>
-				<?php echo $this->form->renderField('nome'); ?>
-				<?php echo $this->form->renderField('email'); ?>
-				<?php echo $this->form->renderField('celular'); ?>
+				<?php echo $this->form->renderField('state'); ?>
 				<?php echo $this->form->renderField('evento_id'); ?>
+				<?php echo $this->form->renderField('nome'); ?>
+				<?php echo $this->form->renderField('celular'); ?>
+				<?php echo $this->form->renderField('email'); ?>
 				<?php
-				foreach((array)$this->item->evento_id as $value)
-				{
-					if(!is_array($value))
-					{
-						echo '<input type="hidden" class="evento_id" name="jform[evento_idhidden]['.$value.']" value="'.$value.'" />';
+				foreach ((array)$this->item->evento_id as $value) {
+					if (!is_array($value)) {
+						echo '<input type="hidden" class="evento_id" name="jform[evento_idhidden][' . $value . ']" value="' . $value . '" />';
 					}
 				}
 				?>
@@ -92,16 +84,13 @@ $document->addStyleSheet(Uri::root() . 'media/com_inscritos/css/form.css');
 	<?php echo JHtml::_('bootstrap.endTab'); ?>
 	<input type="hidden" name="jform[id]" value="<?php echo $this->item->id; ?>" />
 	<input type="hidden" name="jform[ordering]" value="<?php echo $this->item->ordering; ?>" />
-	<input type="hidden" name="jform[state]" value="<?php echo $this->item->state; ?>" />
 	<input type="hidden" name="jform[checked_out]" value="<?php echo $this->item->checked_out; ?>" />
 	<input type="hidden" name="jform[checked_out_time]" value="<?php echo $this->item->checked_out_time; ?>" />
 	<?php echo $this->form->renderField('created_by'); ?>
 	<?php echo $this->form->renderField('modified_by'); ?>
 
-	
 	<?php echo JHtml::_('bootstrap.endTabSet'); ?>
 
-	<input type="hidden" name="task" value=""/>
+	<input type="hidden" name="task" value="" />
 	<?php echo JHtml::_('form.token'); ?>
-
 </form>
